bbd1abc84c7d6ae77b522b2b95fb9a3f1c5045c0
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-25 06:18:53 +0500

    Form Group Created

--- src/app/Pages/sign-in/sign-in.component.ts
+++ src/app/Pages/sign-in/sign-in.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl,FormControlName, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { APIService } from '../../api.service';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
+  constructor(
+    private formBuilder:FormBuilder
+    ){}
 
+  loginForm!:FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group(
+      {
+        Email :["", Validators.required],
+        Password :["", Validators.required],
+      }
+    )
+  }
+  
+  Login(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value)
+      window.alert("malades")
+    }
+    else{
+      window.alert("boshqatadan,davay")
+    }
+  }
 }
