bed7628d80807e0865c34e6d13f5eddebf58418b
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-25 05:57:15 +0500

    That is all

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { SignInComponent } from './Pages/sign-in/sign-in.component';
+import { SignUpComponent } from './Pages/sign-up/sign-up.component';
+import { NotFoundComponent } from './Pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:DashboardComponent},
+    {path:'login',component:SignInComponent},
+    {path:'register',component:SignUpComponent},
+    {path:'**',component:NotFoundComponent},
+];
