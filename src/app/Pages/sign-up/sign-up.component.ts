5896910a3d2495ec17f64ffa53d1f8af9e6a36a5
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-29 00:14:47 +0500

    Registration Page Created

--- src/app/Pages/sign-up/sign-up.component.ts
+++ src/app/Pages/sign-up/sign-up.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { APIService } from '../../api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+  constructor(
+    private formBuilder:FormBuilder,
+    private apiService:APIService,
+    private router:Router
+    ){}
 
+    registerForm!:FormGroup;
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group(
+      {
+        Name :["", Validators.required],
+        Email :["", Validators.required],
+        PhoneNumber :["", Validators.required],
+        Password :["", Validators.required],
+      }
+    )
+  }
+  
+  Register(){
+    if(this.registerForm.valid){
+      console.log(this.registerForm.value)
+      this.apiService.Register(this.registerForm.value)
+      .subscribe({
+        next:(res =>{
+          alert(res.message)
+          this.router.navigate([''])
+        }),
+        error:(err =>{
+          alert(err)
+        })
+      })
+    }
+    else{
+      window.alert("boshqatadan,davay")
+    }
+  }
 }
