127e96bed710c7880d0fd192a886ac9150b1531e
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-30 15:39:58 +0500

    Chiloli qib qo'ydim User listni

--- src/app/Interceptors/token.interceptor.ts
+++ src/app/Interceptors/token.interceptor.ts
@@ -1,15 +1,27 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { APIService } from '../Services/api.service';
 import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
-export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+export const tokenInterceptor: HttpInterceptorFn = (req:HttpRequest<unknown>, next:HttpHandlerFn) => {
   const authService = inject(APIService);
   const myToken = authService.getToken();
+  const router = inject(Router)
 
   if(myToken){
     req = req.clone({
       setHeaders:({Authorization:`Bearer ${myToken}`})
     })
   }
-  return next(req);
+  return next(req).pipe(
+    catchError((err:any)=>{
+      if(err instanceof HttpErrorResponse){
+        if(err.status === 401){
+          router.navigate(['login'])
+        }
+      }
+      return throwError(() => new Error("Nimadur xato ketdida"));
+    })
+  );
 };
